001d2a283205f346ef3efb39656f86e876b55d29
ammaster10s <55087560+ammaster10s@users.noreply.github.com>
2023-07-21 22:23:25 +0700

    add geolocation

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Use [`expo-router`](https://expo.github.io/router) to build native navigation us
 ```sh
  npx create-expo-app@latest --example with-router  -- already done
  npm install expo-font axios react-native-dotenv  → Must Run after git clone
+ npm install react-native-maps  ->> ใช้แผนที่ Map-view
+ npm install react-native-geolocation-service
  npm start → Start virtual machine
 ```
 
@@ -14,4 +16,6 @@ Use [`expo-router`](https://expo.github.io/router) to build native navigation us
 
 - [Expo Router: Docs](https://expo.github.io/router)
 - [Expo Router: Repo](https://github.com/expo/router)
-- [react native map](https://github.com/react-native-maps/react-native-maps)
+- [React native map](https://github.com/react-native-maps/react-native-maps)
+
+
--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image, Text } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const MapScreen = () => {
   const initialRegion = {
-    latitude: 14.073580191499046, // Latitude of the location
-    longitude: 100.60348190755522, // Longitude of the location
-    latitudeDelta: 0.004, // Zoom level latitude
-    longitudeDelta: 0.004, // Zoom level longitude
+    latitude: 14.073580191499046,
+    longitude: 100.60348190755522,
+    latitudeDelta: 0.004,
+    longitudeDelta: 0.004,
   };
 
   const additionalMarkers = [
@@ -19,28 +20,62 @@ const MapScreen = () => {
     { lat: 14.069038, lng: 100.605457, title: "", description: "" },
     { lat: 14.069116, lng: 100.604331, title: "", description: "" },
     { lat: 14.070193, lng: 100.604266, title: "", description: "" },
-    { lat: 14.070328, lng: 100.616118, title: "End Blue line", description: "" },
-  ];
+    { lat: 14.070328, lng: 100.616118, title: "End Blue line", description: "" },];
 
   const routeCoordinates = additionalMarkers.map((marker) => ({
     latitude: marker.lat,
     longitude: marker.lng,
   }));
 
-  const [mapRegion, setMapRegion] = useState(initialRegion);
+  const [userLocation, setUserLocation] = useState(null);
 
-  const onRegionChange = (region) => {
-    // Update the map region when the user pans or zooms
-    setMapRegion(region);
+  const getCurrentLocation = async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        console.log('Location permission not granted');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+      
+      setUserLocation({ latitude, longitude });
+    } catch (error) {
+      console.log('Error getting location:', error.message);
+    }
   };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={initialRegion}
-        onRegionChange={onRegionChange}
+        region={
+          userLocation
+            ? {
+                latitude: userLocation.latitude,
+                longitude: userLocation.longitude,
+                latitudeDelta: 0.004,
+                longitudeDelta: 0.004,
+              }
+            : initialRegion
+        }
       >
+        {userLocation && (
+          <Marker
+            coordinate={{
+              latitude: userLocation.latitude,
+              longitude: userLocation.longitude,
+            }}
+            title="Your Location"
+            description="You are here!"
+          />
+        )}
         {additionalMarkers.map((marker, index) => (
           <Marker
             key={index}
@@ -56,7 +91,11 @@ const MapScreen = () => {
         />
       </MapView>
       {/* Moving Legend */}
-      {/* ... (your existing legend code) */}
+      <View style={styles.legendContainer}>
+        {/* Add your legend components here */}
+        <Text style={styles.legendText}>Legend</Text>
+        {/* ... (your other legend components) */}
+      </View>
     </View>
   );
 };
@@ -75,6 +114,8 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderWidth: 1,
     borderColor: 'gray',
+    bottom: 20, // Adjust this value to position the legend as per your requirements
+    left: 10, // Adjust this value to position the legend as per your requirements
   },
   legendText: {
     fontSize: 16,
